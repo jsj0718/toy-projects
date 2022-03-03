67768ca3de62659ce204af10811f66a4973c3ea1
jsj0718 <jeongsejin718@gmail.com>
2022-03-03 20:21:15 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
 
-CURRENT_PID=$(pgrep -f1 springboot-aws-project | grep jar | aws '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-aws-project | grep jar | awk '{print $1}')
 
 echo ">현재 구종 중인 애플리케이션 PID: $CURRENT_PID"
 
