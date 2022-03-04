5df04d5a22a5b91723db286bb6db287f2570d3aa
jsj0718 <jeongsejin718@gmail.com>
2022-03-04 19:29:22 +0900

    build script 수정 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar | grep jar | grep java | awk '{print $2}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo ">현재 구종 중인 애플리케이션 PID: $CURRENT_PID"
 
